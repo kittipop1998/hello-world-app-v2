40d04fd63ca7a64879f8f6ed2c1c436849681178
Wine Kittipop <37920511+kittipop1998@users.noreply.github.com>
2020-09-17 14:22:51 +0700

    commit

--- App.js
+++ App.js
@@ -1,13 +1,35 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React,{useState} from 'react';
+import { 
+  StyleSheet, 
+  Text, 
+  View,
+  Image, 
+  TextInput, 
+  Button,} from 'react-native';
 
 export default function App() {
+  const [ input, setInput] = useState('')
+  const [color, setColor] = useState('Red')
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Image
+        style={styles.logo}
+        source={{ uri: 'https://www.igeargeek.com/_nuxt/img/835647d.png'}}/> 
+        <Text style={styles.title}>Hello World</Text>
+        <TextInput value={input}
+        onChangeText = {(text) => {setInput(text)}}
+        placeholder="Input here" style={styles.input} />
+        <Text style={{fontSize:18, marginTop:20, color: color}}
+        >
+          {input}
+          </Text>
+        <View style={styles.buttonLayout}>
+          <Button title="Red" color="red" onPress={() => {setColor('red')}}/>
+          <Button title="Blue" color="blue"onPress={() => {setColor('blue')}} />
+          <Button title="Green" color="green" onPress={() => {setColor('green')}}/>
+        </View>
     </View>
+    
   );
 }
 
@@ -17,5 +39,28 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+    padding : 30
+  },
+  logo: {
+    width: 150,
+    height: 150,
+  },
+  title:{
+    'fontSize':24,
+    fontWeight:'bold',
+    marginTop:20,
+  },
+  input:{
+    height: 40,
+    borderColor:'gray',
+    borderStyle:'solid',
+    borderWidth :1,
+    width: '100%',
+    marginTop: 20,
+  },
+  buttonLayout: {
+    flexDirection: 'row',
+    marginTop: 20,
+
   },
 });
