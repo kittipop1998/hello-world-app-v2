c2eac92015169b61ea5d50d5fb79d93f44eee847
Wine Kittipop <37920511+kittipop1998@users.noreply.github.com>
2020-09-17 14:27:44 +0700

    Update App.js

--- App.js
+++ App.js
@@ -1,35 +1,26 @@
-import React,{useState} from 'react';
-import { 
-  StyleSheet, 
-  Text, 
-  View,
-  Image, 
-  TextInput, 
-  Button,} from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, Text, View, TextInput, Button, Image } from 'react-native';
 
 export default function App() {
-  const [ input, setInput] = useState('')
-  const [color, setColor] = useState('Red')
+  const [input, setInput] = useState('')
+  const [textColor, setTextColor] = useState('black')
+
   return (
     <View style={styles.container}>
-      <Image
-        style={styles.logo}
-        source={{ uri: 'https://www.igeargeek.com/_nuxt/img/835647d.png'}}/> 
-        <Text style={styles.title}>Hello World</Text>
-        <TextInput value={input}
-        onChangeText = {(text) => {setInput(text)}}
-        placeholder="Input here" style={styles.input} />
-        <Text style={{fontSize:18, marginTop:20, color: color}}
-        >
-          {input}
-          </Text>
-        <View style={styles.buttonLayout}>
-          <Button title="Red" color="red" onPress={() => {setColor('red')}}/>
-          <Button title="Blue" color="blue"onPress={() => {setColor('blue')}} />
-          <Button title="Green" color="green" onPress={() => {setColor('green')}}/>
-        </View>
+      <Image source={{ uri: 'https://www.igeargeek.com/_nuxt/img/835647d.png' }} style={styles.logo} />
+      <Text style={styles.title}>Hello World</Text>
+      <TextInput
+        value={input} onChangeText={(text) => setInput(text)}
+        placeholder="Input here"
+        style={styles.input}
+      />
+      <Text style={{ color: textColor, fontSize: 18 }}>{input}</Text>
+      <View style={styles.buttonLayout} >
+        <Button title="Red" color="red" onPress={() => setTextColor('red')} />
+        <Button title="Blue" color="blue" onPress={() => setTextColor('blue')} />
+        <Button title="Green" color="green" onPress={() => setTextColor('green')} />
+      </View>
     </View>
-    
   );
 }
 
@@ -39,28 +30,28 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
-    padding : 30
+    padding: 30
   },
   logo: {
     width: 150,
     height: 150,
+    marginBottom: 20
   },
-  title:{
-    'fontSize':24,
-    fontWeight:'bold',
-    marginTop:20,
+  title: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    marginBottom: 20
   },
-  input:{
+  input: {
     height: 40,
-    borderColor:'gray',
-    borderStyle:'solid',
-    borderWidth :1,
+    borderColor: 'gray',
+    borderStyle: 'solid',
+    borderWidth: 1,
     width: '100%',
-    marginTop: 20,
+    marginBottom: 20
   },
   buttonLayout: {
     flexDirection: 'row',
-    marginTop: 20,
-
-  },
+    marginTop: 20
+  }
 });
